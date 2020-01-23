1dbe28f2fad7c945dac18061e5f33ce608b55df9
Jueun-Park <elpion19@gmail.com>
2020-01-23 12:09:31 +0900

    train 4 driving agents

--- train_allvae_driving_agent_script.py
+++ train_allvae_driving_agent_script.py
@@ -2,7 +2,7 @@ import os
 
 
 env_list = ["dc", "df", "nc", "nf"]  # 4 single models
-tensorboard_dir = "stacked_single_model_tensorboard/"
+tensorboard_dir = "allvae32_single_model_tensorboard/"
 # 128 000 timesteps: about 2h, total 2h * 4 = 8h
 timesteps = "128000"
 
@@ -10,4 +10,4 @@ for env in env_list:
     print("Waiting next env", env, "> ", end="")
     a = input()
     print("Start next env > ", env)
-    os.system("python train.py --algo sac --vae-path logs_dncf_stacked/vae-64.pkl -n " + timesteps + " --log-folder logs_" + env + "_stacked/ -tb " + tensorboard_dir)
+    os.system("python train.py --algo sac --vae-path logs_dncf/vae-32.pkl -n " + timesteps + " --log-folder logs_" + env + "_allvae32/ -tb " + tensorboard_dir)
